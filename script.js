b3328b3735c1ebf0a7599a5369ac266f16883569
SiddharthD <96356209+siddharthd0@users.noreply.github.com>
2022-04-20 05:31:06 +0000

    added animation

---
+++ script.js
@@ -0,0 +1,29 @@
+var $ = function(id) {
+  return document.getElementById(id);
+};
+var inc = 0;
+var out = 0;
+var str = 'Siddharth Duggal';
+var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@$%&';
+var t;
+
+var anim = function() {
+  inc++;
+  if (inc % 7 === 0 && out < str.length) {
+    $('anim').appendChild(document.createTextNode(str[out]));
+    out++;
+  } else if (out >= str.length) {
+    $('shuffle').innerHTML = '';
+    removeInterval(t);
+  }
+  $('shuffle').innerHTML =
+    chars[Math.floor(Math.random() * chars.length)];
+};
+t = setInterval(anim, 40);
+$('anim').innerHTML = '';
+
+//Do it again
+function reload() {
+  window.location.href = window.location.href;
+}
+document.getElementById('btn').onclick = reload;
