aa97f232704731c63b87c94055214e5c1cfcb161
SiddharthD <96356209+siddharthd0@users.noreply.github.com>
2022-04-27 06:43:34 +0000

    yes

--- resume/login.js
+++ resume/login.js
@@ -1,10 +1,11 @@
 function checkPswd() {
-        var confirmPassword = "testinglol";
+        var confirmPassword = "resumepassword39";
+    // imagine viewing the source code to get the password, wowoowowow. but whatever idrc
         var password = document.getElementById("pswd").value;
         if (password == confirmPassword) {
              window.location="resume.html";
         }
         else{
-            alert("sorry! password was incorrect, please try again :)");
+            alert("Sorry! The password was incorrect, please try again or contact Siddharth for a new password.");
         }
     }
