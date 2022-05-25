5b39e456886af8ae7fd5f237f171e933af95a278
SiddharthD <96356209+siddharthd0@users.noreply.github.com>
2022-05-25 21:52:40 +0000

    About section added

--- script.js
+++ script.js
@@ -27,3 +27,21 @@ function reload() {
   window.location.href = window.location.href;
 }
 document.getElementById('btn').onclick = reload;
+
+
+window.addEventListener("load", function() {
+  const form = document.getElementById('my-form');
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Success!");
+    })
+  });
+});
+
