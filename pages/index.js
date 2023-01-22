9fca62b431f0cba37c92bd81139732e366c08af9
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-01-22 16:08:32 -0700

    Updated resume

--- pages/index.js
+++ pages/index.js
@@ -129,7 +129,7 @@ export default function Home() {
                   />
                 }
               >
-                Resume
+                Résumé
               </Button>
             </Link>
           </Tooltip>
