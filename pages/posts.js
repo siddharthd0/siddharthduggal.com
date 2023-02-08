f3a44febdbdba5cfd3f8625c76c2c3c74e760861
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-02-08 14:33:54 -0700

    ui

--- pages/posts.js
+++ pages/posts.js
@@ -7,6 +7,7 @@ import {
   Heading,
   BreadcrumbLink,
   BreadcrumbItem,
+  SimpleGrid,
 } from "@chakra-ui/react";
 import ExperienceCard from "../components/experience-card";
 import { BsLinkedin, BsGithub } from "react-icons/bs";
@@ -43,41 +44,49 @@ export default function ProjectPage({ posts }) {
             </BreadcrumbItem>
 
             <BreadcrumbItem isCurrentPage>
-              <BreadcrumbLink href="#">experience</BreadcrumbLink>
+              <BreadcrumbLink href="#">posts</BreadcrumbLink>
             </BreadcrumbItem>
           </Breadcrumb>
           <Spacer />
         </Flex>
-        <div className="announcements-container">
-          <ol className="announcements">
-            {posts.reverse().map((post) => {
-              const date = new Date(post.last_edited_time).toLocaleString(
-                "en-US",
-                {
-                  month: "short",
-                  day: "2-digit",
-                  year: "numeric",
-                }
-              );
+        <SimpleGrid columns={{ sm: 2, md: 3 }} spacing="14px">
+          {posts.reverse().map((post) => {
+            const date = new Date(post.last_edited_time).toLocaleString(
+              "en-US",
+              {
+                month: "short",
+                day: "2-digit",
+                year: "numeric",
+              }
+            );
 
-              return (
-                <Box marginTop={"1rem"} className="tinted-bg" key={post.id}>
-                  <h3 className="postTitle">
-                    <Text text={post.properties.Name.title} />
-                  </h3>
+            return (
+              <Box
+                borderRadius={"10px"}
+                py="10px"
+                px="1rem"
+                marginTop={"1rem"}
+               backgroundColor="gray.900"
+               _hover={{
+                backgroundColor: "gray.800",
+               }}
+                key={post.id}
+              >
+                <h3 className="postTitle">
+                  <Text text={post.properties.Name.title} />
+                </h3>
 
-                  <p className="postDescription">
-                    <Text text={post.properties.Description.text} />
-                  </p>
-                  <p className="postDescription">{date}</p>
-                  <Link className="readMoreLink" href={`/${post.id}`}>
-                    Read more →{" "}
-                  </Link>
-                </Box>
-              );
-            })}
-          </ol>
-        </div>
+                <p className="postDescription">
+                  <Text text={post.properties.Description.text} />
+                </p>
+                <p className="postDescription">{date}</p>
+                <Link className="readMoreLink" href={`/${post.id}`}>
+                  Read more →{" "}
+                </Link>
+              </Box>
+            );
+          })}
+        </SimpleGrid>
       </Flex>
 
       <Box px="2rem" maxW={"680px"} margin="auto">
