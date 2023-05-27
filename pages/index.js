44bf54754749f4145213114aaadbfdaa16539adf
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-05-27 13:58:40 -0600

    animation

--- pages/index.js
+++ pages/index.js
@@ -34,22 +34,28 @@ import { FaTwitter, FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
 export const databaseId = process.env.NOTION_DATABASE_ID;
 
 export default function Home({ posts }) {
-  const buttonVariants = {
-    hidden: {
-      y: 50,
-      opacity: 0,
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        delayChildren: 1,
+        staggerChildren: 0.2
+      }
     },
+  };
+  
+  const childVariants = {
+    hidden: { y: 20, opacity: 0 },
     visible: {
       y: 0,
       opacity: 1,
       transition: {
-        type: "spring",
-        damping: 20,
-        stiffness: 100,
+        y: { stiffness: 1000, velocity: -100 },
       },
     },
   };
-  const buttonDelay = 0.15;
+  
   return (
     <>
       <Head>
@@ -65,142 +71,150 @@ export default function Home({ posts }) {
         maxW={"400px !important"}
       >
         <Header />
-        <Flex alignItemss="center" pb="10px" mt="2rem !important">
-          <Heading fontSize="xl" fontWeight="500" color="whiteAlpha.800">
-            Intro
-          </Heading>
-          <Spacer />
-          <HStack spacing={3}>
-            <Link
-              _hover={{
-                color: "twitter.500 !important",
-              }}
-              href="https://twitter.com/siddharthd01"
-              isExternal
-              color="whiteAlpha.500"
-            >
-              <FaTwitter size={20} />
-            </Link>
-            <Link
-              _hover={{
-                color: "red.500 !important",
-              }}
-              href="https://www.instagram.com/siddharth.duggal/"
-              isExternal
-              color="whiteAlpha.500"
-            >
-              <FaInstagram size={20} />
-            </Link>
-            <Link
-              _hover={{
-                color: "linkedin.500 !important",
-              }}
-              href="https://www.linkedin.com/in/siddharth-duggal/"
-              isExternal
-              color="whiteAlpha.500"
-            >
-              <FaLinkedin size={20} />
-            </Link>
-            <Link
-              _hover={{
-                color: "gray.200 !important",
-              }}
-              href="https://github.com/siddharthd0"
-              isExternal
-              color="whiteAlpha.500"
-            >
-              <FaGithub size={20} />
-            </Link>
-          </HStack>
-        </Flex>
-        <chakra.p color="whiteAlpha.700" fontSSize="xs">
-          I&apos;m a 17-year-old based in the USA, deeply interested in tech. I
-          love creating things that can help people. Currently, I&apos;m working
-          on{" "}
-          <Link href="https://techoptimum.org" textDecoration="none !important">
-            Tech Optimum
-          </Link>
-          . Tech Optimum is a nonprofit committed to offering free programming
-          education, striving to be the all-in-one platform for students eager
-          to dive into the world of tech.
-        </chakra.p>
-
-        <Heading
-          mt="2rem !important"
-          fontSize="xl"
-          fontWeight="500"
-          color="whiteAlpha.800"
-          pb="10px"
-        >
-          Posts
-        </Heading>
-        <SimpleGrid>
-          {posts.map((post) => {
-            const date = new Date(post.last_edited_time).toLocaleString(
-              "en-US",
-              {
-                month: "2-digit",
-                day: "2-digit",
-                year: "2-digit",
-              }
-            );
-
-            return (
-              <Link textDecoration={"none !important"} href={`/${post.id}`}>
-                <Box transition={"200ms"} borderRadius={"9px"} role="group">
-                  <Box
-                    transition={"300ms"}
-                    pb="18px"
-                    borderRadius={"6px"}
-                    key={post.id}
-                    _hover={{
-                      color: "white",
-                      transform: "scale(1.1)",
-                    }}
-                  >
-                    <Flex alignItems="center">
-                      <Heading
-                        color="whiteAlpha.600 !important"
-                        fontWeight={"500"}
-                        fontSize="md"
-                        transition={"300ms"}
-                        _groupHover={{
-                          color: "blue.500 !important",
-                        }}
-                      >
-                        <Text text={post.properties.Name.title} />
-                      </Heading>
-                      <Spacer />
-                      <Heading
-                        transition={"300ms"}
-                        pl="1rem"
-                        color="whiteAlpha.500"
-                        mt="6px"
-                        fontSize="xs"
-                        fontWeight={"thin"}
-                        className="postDescription"
-                        _groupHover={{
-                          color: "blue.700 !important",
-                        }}
-                      >
-                        {date}
-                      </Heading>
-                    </Flex>
-                  </Box>
-                </Box>
+        <motion.div variants={containerVariants} initial="hidden" animate="visible">
+          <Flex as={motion.div} variants={childVariants} alignItemss="center" pb="10px" mt="2rem !important">
+            <Heading fontSize="xl" fontWeight="500" color="whiteAlpha.800">
+              Intro
+            </Heading>
+            <Spacer />
+            <HStack spacing={3}>
+              <Link
+                _hover={{
+                  color: "twitter.500 !important",
+                }}
+                href="https://twitter.com/siddharthd01"
+                isExternal
+                color="whiteAlpha.500"
+              >
+                <FaTwitter size={20} />
               </Link>
-            );
-          })}
-        </SimpleGrid>
-        <Heading
-          mt="2rem !important"
-          fontSize="xl"
-          fontWeight="500"
-          color="whiteAlpha.800"
-        >
-          Projects
-        </Heading>
-        <Flex direction={"column"}>
+              <Link
+                _hover={{
+                  color: "red.500 !important",
+                }}
+                href="https://www.instagram.com/siddharth.duggal/"
+                isExternal
+                color="whiteAlpha.500"
+              >
+                <FaInstagram size={20} />
+              </Link>
+              <Link
+                _hover={{
+                  color: "linkedin.500 !important",
+                }}
+                href="https://www.linkedin.com/in/siddharth-duggal/"
+                isExternal
+                color="whiteAlpha.500"
+              >
+                <FaLinkedin size={20} />
+              </Link>
+              <Link
+                _hover={{
+                  color: "gray.200 !important",
+                }}
+                href="https://github.com/siddharthd0"
+                isExternal
+                color="whiteAlpha.500"
+              >
+                <FaGithub size={20} />
+              </Link>
+            </HStack>
+          </Flex>
+          <Box as={motion.div} variants={childVariants}>
+          <chakra.p color="whiteAlpha.700" fontSize="sm">
+            I&apos;m a 17-year-old based in the USA, deeply interested in tech. I
+            love creating things that can help people. Currently, I&apos;m working
+            on{" "}
+            <Link href="https://techoptimum.org" textDecoration="none !important">
+              Tech Optimum
+            </Link>
+            . Tech Optimum is a nonprofit committed to offering free programming
+            education, striving to be the all-in-one platform for students eager
+            to dive into the world of tech.
+          </chakra.p>
+          </Box>
+         
+          <Heading
+            mt="2rem !important"
+            fontSize="xl"
+            fontWeight="500"
+            color="whiteAlpha.800"
+            pb="10px"
+            as={motion.div}
+            variants={childVariants}
+          >
+            Posts
+          </Heading>
+          <SimpleGrid as={motion.div} variants={childVariants}>
+            {posts.map((post) => {
+              const date = new Date(post.last_edited_time).toLocaleString(
+                "en-US",
+                {
+                  month: "2-digit",
+                  day: "2-digit",
+                  year: "2-digit",
+                }
+              );
+
+              return (
+                <Link textDecoration={"none !important"} href={`/${post.id}`}>
+                  <Box transition={"200ms"} borderRadius={"9px"} role="group">
+                    <Box
+                      transition={"300ms"}
+                      pb="18px"
+                      borderRadius={"6px"}
+                      key={post.id}
+                      _hover={{
+                        color: "white",
+                        transform: "scale(1.1)",
+                      }}
+                    >
+                      <Flex alignItems="center">
+                        <Heading
+                          color="whiteAlpha.600 !important"
+                          fontWeight={"500"}
+                          fontSize="md"
+                          transition={"300ms"}
+                          _groupHover={{
+                            color: "blue.500 !important",
+                          }}
+                        >
+                          <Text text={post.properties.Name.title} />
+                        </Heading>
+                        <Spacer />
+                        <Heading
+                          transition={"300ms"}
+                          pl="1rem"
+                          color="whiteAlpha.500"
+                          mt="6px"
+                          fontSize="xs"
+                          fontWeight={"thin"}
+                          className="postDescription"
+                          _groupHover={{
+                            color: "blue.700 !important",
+                          }}
+                        >
+                          {date}
+                        </Heading>
+                      </Flex>
+                    </Box>
+                  </Box>
+                </Link>
+              );
+            })}
+          </SimpleGrid>
+          <Heading
+          as={motion.div}
+          variants={childVariants}
+            mt="2rem !important"
+            fontSize="xl"
+            fontWeight="500"
+            color="whiteAlpha.800"
+          >
+            Projects
+          </Heading>
+          <Flex as={motion.div} variants={childVariants} direction={"column"}>
+           
           <Project
             link="https://resumate.tech"
             title="Resumate"
@@ -280,14 +294,14 @@ export default function Home({ posts }) {
           lang="en"
           loading="lazy"
         />
-
+        </motion.div>
         <Footer />
       </Flex>
     </>
   );
 }
 
-export const getStaticProps = async () => {
+export async function getStaticProps() {
   const database = await getDatabase(databaseId);
   return {
     props: {
@@ -295,4 +309,4 @@ export const getStaticProps = async () => {
     },
     revalidate: 1,
   };
-};
+}
