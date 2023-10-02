3127c9b0d234a590a6d794bd19f53b46e4629f3a
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-10-02 13:17:35 -0500

    redirects for projects

--- next.config.js
+++ next.config.js
@@ -1,7 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
+  async redirects() {
+    return [
+      {
+        source: '/career4all',
+        destination: 'https://devpost.com/software/career4all',
+        permanent: false,
+      },
+      {
+        source: '/skyline',
+        destination: 'https://devpost.com/software/skyline-a9084w',
+        permanent: false,
+      },
+      {
+        source: '/carbonara',
+        destination: 'https://devpost.com/software/carbonara-8491qs',
+        permanent: false,
+      },
+      {
+        source: '/aeolus',
+        destination: 'https://devpost.com/software/aeolus',
+        permanent: false,
+      },
+      {
+        source: '/devpost',
+        destination: 'https://www.devpost.com/siddharthduggal2013',
+        permanent: false,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
