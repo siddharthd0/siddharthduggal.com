d239c8db0e22d49be2293b01adc5623d58f6eb0a
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-11-22 18:50:02 -0700

    deploy + million

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
+const million = require('million/compiler');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
-module.exports = {
+module.exports = million.next(
+  {
   env: {
     NEXT_PUBLIC_CLERK_FRONTEND_API: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API,
   },
-};
+}, { auto: true }
+);
 
 
 module.exports = nextConfig
--- src/pages/api/sendMessage.js
+++ src/pages/api/sendMessage.js
@@ -1,25 +1,23 @@
-// pages/api/sendMessage.js
 export default async function handler(req, res) {
-    if (req.method === 'POST') {
-      try {
-        const webhookUrl = process.env.DISCORD;
-        const response = await fetch(webhookUrl, {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(req.body),
-        });
-  
-        if (!response.ok) {
-          throw new Error(`Webhook responded with status ${response.status}`);
-        }
-  
-        res.status(200).json({ message: 'Message sent successfully' });
-      } catch (error) {
-        res.status(500).json({ message: error.message });
+  if (req.method === "POST") {
+    try {
+      const webhookUrl = process.env.DISCORD;
+      const response = await fetch(webhookUrl, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(req.body),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Webhook responded with status ${response.status}`);
       }
-    } else {
-      res.setHeader('Allow', ['POST']);
-      res.status(405).json({ message: `Method ${req.method} Not Allowed` });
+
+      res.status(200).json({ message: "Message sent successfully" });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).json({ message: `Method ${req.method} Not Allowed` });
   }
-  
+}
