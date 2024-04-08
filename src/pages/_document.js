aa22e258d57a2d609629961d507367a69a1a8ed9
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2024-04-07 22:26:29 -0600

    tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <meta property="og:title" content="Siddharth Duggal - Portfolio" />
+          <meta property="og:description" content="Entrepreneur, experimenting with AI" />
+          <meta property="og:image" content="https://siddharthduggal.com/siddharth-cover.png" />
+          <meta property="og:url" content="https://siddharthduggal.com" />
+          <meta property="og:type" content="website" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Siddharth Duggal" />
+          <meta name="twitter:description" content="Entrepreneur, experimenting with AI" />
+          <meta name="twitter:image" content="https://siddharthduggal.com/siddharth-cover.png" />
+        </Head>
       <body>
         <Main />
         <NextScript />
